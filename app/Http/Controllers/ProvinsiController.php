ca8827c8a6304af5dece353b0b997a043c975191
yumiyaCode <arcadianew11@gmail.com>
2021-02-26 19:22:40 +0700

    host

---
+++ app/Http/Controllers/ProvinsiController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Provinsi;
+use App\Http\Controllers\DB;
+use Illuminate\Http\Request;
+
+class ProvinsiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $provinsi = Provinsi::all();
+        return view('provinsi.index',compact('provinsi'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('provinsi.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kode_provinsi' => 'required|max:4|unique:provinsis',
+            'nama_provinsi' => 'required|unique:provinsis'
+
+        ],
+        [
+            'kode_provinsi.required' => 'Kode Harap Diisi!',
+            'kode_provinsi.max' => 'Kode Max 4 Digit',
+            'kode_provinsi.unique' => 'Kode Sudah Terpakai',
+            'nama_provinsi.required' => 'Nama Provinsi Harap Diisi!',
+            'nama_provinsi.unique' => 'Nama Sudah Terpakai'
+
+        ]);
+        $provinsi = new Provinsi();
+        $provinsi->kode_provinsi = $request->kode_provinsi;
+        $provinsi->nama_provinsi = $request->nama_provinsi;
+        $provinsi->save();
+        return redirect()->route('provinsi.index')
+                        ->with(['message'=>'provinsi Berhasil dibuat']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Provinsi  $provinsi
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $provinsi = Provinsi::findOrFail($id);
+        return view('provinsi.show',compact('provinsi'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Provinsi  $provinsi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $provinsi = Provinsi::findOrFail($id);
+        return view('provinsi.edit',compact('provinsi'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Provinsi  $provinsi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $request->validate([
+            'kode_provinsi' => 'required|max:4',
+            'nama_provinsi' => 'required'
+
+        ],
+        [
+            'kode_provinsi.required' => 'Kode Harap Diisi!',
+            'kode_provinsi.max' => 'Kode Max 4 Digit',
+            'nama_provinsi.required' => 'Nama Provinsi Harap Diisi!'
+
+        ]);
+        $provinsi = Provinsi::findOrFail($id);
+        $provinsi->kode_provinsi = $request->kode_provinsi;
+        $provinsi->nama_provinsi = $request->nama_provinsi;
+        $provinsi->save();
+        return redirect()->route('provinsi.index')
+                        ->with(['message'=>'provinsi Berhasil diedit']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Provinsi  $provinsi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $provinsi = Provinsi::findOrFail($id)->delete();
+        return redirect()->route('provinsi.index')
+                        ->with(['message1'=>'Berhasil dihapus']);
+    }
+}
---
+++ app/Http/Controllers/RwController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rw;
+use App\Models\Kelurahan;
+use App\Http\Controllers\DB;
+use Illuminate\Http\Request;
+
+class RwController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $rw = Rw::with('kelurahan')->get();
+        return view('rw.index',compact('rw'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $kelurahan = Kelurahan::all();
+        return view('rw.create',compact('kelurahan'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_rw' => 'required|max:3'
+
+        ],
+        [
+            'nama_rw.required' => 'Nama rw Harap Diisi!',
+            'nama_rw.max' => 'Maksimal 3 Digit!'
+
+        ]);
+
+        $rw= new Rw();
+        $rw->nama_rw = $request->nama_rw;
+        $rw->id_kelurahan = $request->id_kelurahan;
+        $rw->save();
+        return redirect()->route('rw.index')
+            ->with(['message'=>'Data Berhasil dibuat']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Rw  $rw
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $rw = Rw::findOrFail($id);
+        return view('rw.show',compact('rw'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Rw  $rw
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {  
+        $kelurahan = Kelurahan::all();
+        $rw = Rw::findOrFail($id);
+        return view('rw.edit',compact('rw','kelurahan'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Rw  $rw
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama_rw' => 'required'
+
+        ],
+        [
+            'nama_rw.required' => 'Nama rw Harap Diisi!',
+
+        ]);
+        $rw = Rw::findOrFail($id);
+        $rw->nama_rw = $request->nama_rw;
+        $rw->id_kelurahan = $request->id_kelurahan;
+        $rw->save();
+        return redirect()->route('rw.index')
+            ->with(['message'=>'Data Berhasil diedit']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Rw  $rw
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $rw = Rw::findOrFail($id)->delete();
+        return redirect()->route('rw.index')
+                        ->with(['message1'=>'Berhasil dihapus']);
+    }
+}
